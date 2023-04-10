a5fb207c00e5321aee8f16b39bd3661ecba5f938
sumanbaul <musique.suman@gmail.com>
2023-04-11 00:03:25 +0530

    Dashboard not working

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@
 #include <ThingsBoard.h>
 #include <L293D.h>
 
-
 /*
 
   Project: Main door lock
@@ -22,10 +21,10 @@
 const char *serverName = "https://automation.mindflo.in/esp-outputs-action.php?action=outputs_state&board=1";
 
 // thingsboard variables
-#define TOKEN "5jeTWUk1oxSHUlqnr1ol" // "lf2p7b4pfltZghdVevqB"//"5jeTWUk1oxSHUlqnr1ol"
+#define TOKEN "5jeTWUk1oxSHUlqnr1ol"        // "lf2p7b4pfltZghdVevqB"//"5jeTWUk1oxSHUlqnr1ol"
 char thingsboardServer[] = "192.168.0.108"; //"demo.thingsboard.io";
 constexpr uint16_t THINGSBOARD_PORT = 9232U;
-//#define THINGSBOARD_PORT 9232
+// #define THINGSBOARD_PORT 9232
 constexpr uint32_t MAX_MESSAGE_SIZE = 256U;
 // initialize thingsboard client
 WiFiClient espClient;
@@ -34,6 +33,12 @@ WiFiClient espClient;
 ThingsBoard tb(espClient, MAX_MESSAGE_SIZE);
 // the wifi radio's status
 int status = WL_IDLE_STATUS;
+
+// Attribute names for attribute request and attribute updates functionality
+
+constexpr char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
+constexpr char LED_MODE_ATTR[] = "ledMode";
+constexpr char LED_STATE_ATTR[] = "ledState";
 
 // LEDs
 const int PIN_GREEN = 22;
@@ -44,6 +49,44 @@ const int LEDINBUILT = 2;
 //
 int flag = 0;
 
+// Statuses for subscribing to rpc
+bool subscribed = false;
+
+// handle led state and mode changes
+volatile bool attributesChanged = false;
+
+// LED modes: 0 - continious state, 1 - blinking
+volatile int ledMode = 0;
+
+// Current led state
+volatile bool ledState = false;
+
+// Settings for interval in blinking mode
+constexpr uint16_t BLINKING_INTERVAL_MS_MIN = 10U;
+constexpr uint16_t BLINKING_INTERVAL_MS_MAX = 60000U;
+volatile uint16_t blinkingInterval = 1000U;
+
+uint32_t previousStateChange;
+
+// For telemetry
+constexpr int16_t telemetrySendInterval = 2000U;
+uint32_t previousDataSend;
+
+// List of shared attributes for subscribing to their updates
+constexpr std::array<const char *, 2U> SHARED_ATTRIBUTES_LIST = {
+  LED_STATE_ATTR,
+  BLINKING_INTERVAL_ATTR
+};
+
+// List of client attributes for requesting them (Using to initialize device states)
+constexpr std::array<const char *, 1U> CLIENT_ATTRIBUTES_LIST = {
+  LED_MODE_ATTR
+};
+
+
+
+
+
 // Motors
 //  Update interval time set to 5 seconds
 const long interval = 5000;
@@ -53,17 +96,15 @@ int motor1Pin1 = 27;
 int motor1Pin2 = 26;
 int enable1Pin = 14;
 
-
 // Setting PWM properties
 const int freq = 30000;
 const int pwmChannel = 0;
 const int resolution = 8;
 int dutyCycle = 200;
 
-//Pin 1, 2, 3 and PWM channel 0
+// Pin 1, 2, 3 and PWM channel 0
 L293D motor(motor1Pin1, motor1Pin2, enable1Pin, pwmChannel);
 
-bool subscribed = false;
 const size_t callbacks_size = 1;
 
 void InitWiFi()
@@ -141,39 +182,106 @@ void setup()
   InitWiFi();
   InitLeds();
   InitMotors();
-  
 }
 
-// RPC_Response ts1(const RPC_Data &data)
-// {
-//   Serial.println("Received the set switch method 4!");
-//   char params[10];
-//   serializeJson(data, params);
-//   //Serial.println(params);
-//   String _params = params;
-//   if (_params == "true") {
-//     Serial.println("Toggle Switch - 1 => On");
+void sendDataToThingsBoard()
+{
+  tb.sendTelemetryInt("temperature", random(10, 20));
+  tb.sendTelemetryFloat("humidity", flag);
+  tb.sendAttributeInt("rssi", WiFi.RSSI());
+  tb.sendAttributeString("bssid", WiFi.BSSIDstr().c_str());
+  tb.sendAttributeString("localIp", WiFi.localIP().toString().c_str());
+  tb.sendAttributeString("ssid", WiFi.SSID().c_str());
+  tb.sendAttributeInt("channel", WiFi.channel());
+}
 
-//   }
-//   else  if (_params == "false")  {
-//     Serial.println("Toggle Switch - 1 => Off");
-//   }
 
-// }
+/// @brief Processes function for RPC call "setLedMode"
+/// RPC_Data is a JSON variant, that can be queried using operator[]
+/// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+/// @param data Data containing the rpc data that was called and its current value
+/// @return Response that should be sent to the cloud. Useful for getMethods
+RPC_Response processSetLedMode(const RPC_Data &data) {
+  Serial.println("Received the set led state RPC method");
 
-// RPC_Callback callbacks[callbacks_size] = {
-//   { "getValue_1", ts1 }   // enter the name of your switch variable inside the string1
-// };
+  // Process data
+  int new_mode = data;
+
+  Serial.print("Mode to change: ");
+  Serial.println(new_mode);
+
+  if (new_mode != 0 && new_mode != 1) {
+    StaticJsonDocument<200> doc;
+    doc["error"] = "Unknown mode!";
+    //return RPC_Response(doc.as<JsonVariant>());
+    return RPC_Response(doc.as<JsonVariant>());
+  }
+
+  ledMode = new_mode;
+
+  attributesChanged = true;
+
+  // Returning current mode
+  StaticJsonDocument<200> doc;
+  doc["newMode"] = (int)ledMode;
+
+  return RPC_Response(doc.as<JsonVariant>());
+}
+
+
+// Optional, keep subscribed shared attributes empty instead,
+// and the callback will be called for every shared attribute changed on the device,
+// instead of only the one that were entered instead
+const std::array<RPC_Callback, 1U> callbacks = {
+  RPC_Callback{ "setLedMode", processSetLedMode }
+};
+
+
+/// @brief Update callback that will be called as soon as one of the provided shared attributes changes value,
+/// if none are provided we subscribe to any shared attribute change instead
+/// @param data Data containing the shared attributes that were changed and their current value
+void processSharedAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0) {
+      const uint16_t new_interval = it->value().as<uint16_t>();
+      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX) {
+        blinkingInterval = new_interval;
+        Serial.print("Updated blinking interval to: ");
+        Serial.println(new_interval);
+      }
+    } else if(strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
+      ledState = it->value().as<bool>();
+      digitalWrite(LEDINBUILT, ledState ? HIGH : LOW);
+      Serial.print("Updated state to: ");
+      Serial.println(ledState);
+    }
+  }
+  attributesChanged = true;
+}
+
+void processClientAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), LED_MODE_ATTR) == 0) {
+      const uint16_t new_mode = it->value().as<uint16_t>();
+      ledMode = new_mode;
+    }
+  }
+}
+
+const Shared_Attribute_Callback attributes_callback(SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend(), &processSharedAttributes);
+const Attribute_Request_Callback attribute_shared_request_callback(SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend(), &processSharedAttributes);
+const Attribute_Request_Callback attribute_client_request_callback(CLIENT_ATTRIBUTES_LIST.cbegin(), CLIENT_ATTRIBUTES_LIST.cend(), &processClientAttributes);
+
 
 void loop()
 {
   if (WiFi.status() != WL_CONNECTED)
   {
+    subscribed = false;
     reconnect();
   }
 
   Serial.println(WiFi.status());
-
 
   if (!tb.connected())
   {
@@ -183,16 +291,72 @@ void loop()
     Serial.print(thingsboardServer);
     Serial.print(" with token ");
     Serial.println(TOKEN);
-    if (!tb.connect(thingsboardServer,  TOKEN))// THINGSBOARD_PORT))
+    if (!tb.connect(thingsboardServer, TOKEN)) // THINGSBOARD_PORT))
     {
       Serial.println("Failed to connect");
       return;
     }
   }
 
-  tb.sendTelemetryFloat("temperature", flag);
-  tb.sendTelemetryFloat("humidity", flag);
-  flag++;
+////
+if (!subscribed) {
+    Serial.println("Subscribing for RPC...");
+    // Perform a subscription. All consequent data processing will happen in
+    // processSetLedState() and processSetLedMode() functions,
+    // as denoted by callbacks array.
+    if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }
+
+    if (!tb.Shared_Attributes_Subscribe(attributes_callback)) {
+      Serial.println("Failed to subscribe for shared attribute updates");
+      return;
+    }
+
+    Serial.println("Subscribe done");
+    subscribed = true;
+
+    // Request current states of shared attributes
+    if (!tb.Shared_Attributes_Request(attribute_shared_request_callback)) {
+      Serial.println("Failed to request for shared attributes");
+      return;
+    }
+
+    // Request current states of client attributes
+    if (!tb.Client_Attributes_Request(attribute_client_request_callback)) {
+      Serial.println("Failed to request for client attributes");
+      return;
+    }
+  }
+
+  if (attributesChanged) {
+    attributesChanged = false;
+    if (ledMode == 0) {
+      previousStateChange = millis();
+    }
+    tb.sendTelemetryInt(LED_MODE_ATTR, ledMode);
+    tb.sendTelemetryBool(LED_STATE_ATTR, ledState);
+    tb.sendAttributeInt(LED_MODE_ATTR, ledMode);
+    tb.sendAttributeBool(LED_STATE_ATTR, ledState);
+  }
+
+  if (ledMode == 1 && millis() - previousStateChange > blinkingInterval) {
+    previousStateChange = millis();
+    ledState = !ledState;
+    digitalWrite(LEDINBUILT, ledState);
+    tb.sendTelemetryBool(LED_STATE_ATTR, ledState);
+    tb.sendAttributeBool(LED_STATE_ATTR, ledState);
+    if (LEDINBUILT == 99) {
+      Serial.print("LED state changed to: ");
+      Serial.println(ledState);
+    }
+  }
+
+////
+
+
+  sendDataToThingsBoard();
   // if (!subscribed) {
   //   Serial.println("Subscribing for RPC...");
 
@@ -255,7 +419,7 @@ void loop()
   // digitalWrite(motor1Pin1, HIGH);
   // digitalWrite(motor1Pin2, LOW);
 
-  motor.begin();     
+  motor.begin();
   motor.SetMotorSpeed(250);
   delay(5000);
   motor.Stop();
@@ -277,79 +441,3 @@ void loop()
 
   tb.loop();
 }
-
-// String httpGETRequest(const char *serverName)
-// {
-//   WiFiClientSecure *client = new WiFiClientSecure;
-
-//   unsigned long currentMillis = millis();
-//   String outputsState;
-
-//   if (currentMillis - previousMillis >= interval)
-//   {
-//     // Check WiFi connection status
-//     if (WiFi.status() == WL_CONNECTED)
-//     {
-//       outputsState = httpGETRequest(serverName);
-//       Serial.println(outputsState);
-//       JSONVar myObject = JSON.parse(outputsState);
-
-//       // JSON.typeof(jsonVar) can be used to get the type of the var
-//       if (JSON.typeof(myObject) == "undefined")
-//       {
-//         Serial.println("Parsing input failed!");
-//         return "";
-//       }
-
-//       Serial.print("JSON object = ");
-//       Serial.println(myObject);
-
-//       // myObject.keys() can be used to get an array of all the keys in the object
-//       JSONVar keys = myObject.keys();
-
-//       for (int i = 0; i < keys.length(); i++)
-//       {
-//         JSONVar value = myObject[keys[i]];
-//         Serial.print("GPIO: ");
-//         Serial.print(keys[i]);
-//         Serial.print(" - SET to: ");
-//         Serial.println(value);
-//         pinMode(atoi(keys[i]), OUTPUT);
-//         digitalWrite(atoi(keys[i]), atoi(value));
-//       }
-//       // save the last HTTP GET Request
-//       previousMillis = currentMillis;
-//     }
-//     else
-//     {
-//       Serial.println("WiFi Disconnected");
-//     }
-//   }
-//   // set secure client without certificate
-//   client->setInsecure();
-//   HTTP_Client https;
-
-//   // Your IP address with path or Domain name with URL path
-//   https.begin(*client, serverName);
-
-//   // Send HTTP POST request
-//   int httpResponseCode = https.GET();
-
-//   String payload = "{}";
-
-//   if (httpResponseCode > 0)
-//   {
-//     Serial.print("HTTP Response code: ");
-//     Serial.println(httpResponseCode);
-//     payload = https.getString();
-//   }
-//   else
-//   {
-//     Serial.print("Error code: ");
-//     Serial.println(httpResponseCode);
-//   }
-//   // Free resources
-//   https.end();
-
-//   return payload;
-// }
