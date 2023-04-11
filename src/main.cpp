ed016801e205a703e992134a9491e0392eb8dfa4
sumanbaul <musique.suman@gmail.com>
2023-04-11 23:03:54 +0530

    speed and control are restarting esp

--- src/main.cpp
+++ src/main.cpp
@@ -74,18 +74,12 @@ uint32_t previousDataSend;
 
 // List of shared attributes for subscribing to their updates
 constexpr std::array<const char *, 2U> SHARED_ATTRIBUTES_LIST = {
-  LED_STATE_ATTR,
-  BLINKING_INTERVAL_ATTR
-};
+    LED_STATE_ATTR,
+    BLINKING_INTERVAL_ATTR};
 
 // List of client attributes for requesting them (Using to initialize device states)
 constexpr std::array<const char *, 1U> CLIENT_ATTRIBUTES_LIST = {
-  LED_MODE_ATTR
-};
-
-
-
-
+    LED_MODE_ATTR};
 
 // Motors
 //  Update interval time set to 5 seconds
@@ -100,12 +94,12 @@ int enable1Pin = 14;
 const int freq = 30000;
 const int pwmChannel = 0;
 const int resolution = 8;
-int dutyCycle = 200;
-
+int dutyCycle = 255;
+int curState = 1;
 // Pin 1, 2, 3 and PWM channel 0
-L293D motor(motor1Pin1, motor1Pin2, enable1Pin, pwmChannel);
+//L293D motor(motor1Pin1, motor1Pin2, enable1Pin, pwmChannel);
 
-const size_t callbacks_size = 1;
+const size_t callbacks_size = 4;
 
 void InitWiFi()
 {
@@ -147,6 +141,52 @@ void InitLeds()
   // Serial.begin(921600);
 }
 
+void motorStop()
+{
+  Serial.println("Motor stopped");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, LOW);
+  delay(10);
+}
+void motorBackward()
+{
+  Serial.println("Moving Backwards");
+  digitalWrite(motor1Pin1, HIGH);
+  digitalWrite(motor1Pin2, LOW);
+}
+
+void motorForward()
+{
+  Serial.println("Moving Forward");
+  digitalWrite(motor1Pin1, LOW);
+  digitalWrite(motor1Pin2, HIGH);
+}
+
+void setDuty(int dc)
+{
+
+  ledcWrite(pwmChannel, dc);
+  Serial.println("Received Speed is ");
+  Serial.println(dc);
+  if ((dc == 0) || (curState == 0))
+  {
+    motorStop();
+    tb.sendTelemetryInt("State", 0); // motorEdgeSpeed
+    StaticJsonDocument<200> doc;
+    doc["motorEdgeSpeed"] = 0;
+    RPC_Response(doc.as<JsonVariant>());
+  }
+  else if ((dc > 0) && (curState == 2))
+  {
+    motorBackward();
+  }
+  else if ((dc > 0) && (curState == 1))
+  {
+    motorForward();
+  }
+}
+
+
 void InitMotors()
 {
   // sets the pins as outputs:
@@ -159,7 +199,7 @@ void InitMotors()
 
   // attach the channel to the GPIO to be controlled
   ledcAttachPin(enable1Pin, pwmChannel);
-
+ setDuty(dutyCycle); 
   // testing
   Serial.print("Testing DC Motor...");
 }
@@ -171,49 +211,42 @@ void setColor(int R, int G, int B)
   analogWrite(PIN_BLUE, B);
 }
 
-void setup()
-{
-  // Increase internal buffer size after inital creation.
-  tb.setBufferSize(128);
-
-  Serial.begin(115200);
-
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  InitWiFi();
-  InitLeds();
-  InitMotors();
-}
-
 void sendDataToThingsBoard()
 {
-  tb.sendTelemetryInt("temperature", random(10, 20));
-  tb.sendTelemetryFloat("humidity", flag);
-  tb.sendAttributeInt("rssi", WiFi.RSSI());
-  tb.sendAttributeString("bssid", WiFi.BSSIDstr().c_str());
-  tb.sendAttributeString("localIp", WiFi.localIP().toString().c_str());
-  tb.sendAttributeString("ssid", WiFi.SSID().c_str());
-  tb.sendAttributeInt("channel", WiFi.channel());
-}
 
+  // Sending telemetry every telemetrySendInterval time
+  if (millis() - previousDataSend > telemetrySendInterval)
+  {
+    previousDataSend = millis();
+    tb.sendTelemetryInt("temperature", random(10, 20));
+    tb.sendAttributeInt("rssi", WiFi.RSSI());
+    tb.sendAttributeInt("channel", WiFi.channel());
+    tb.sendAttributeString("bssid", WiFi.BSSIDstr().c_str());
+    tb.sendAttributeString("localIp", WiFi.localIP().toString().c_str());
+    tb.sendAttributeString("ssid", WiFi.SSID().c_str());
+  }
+}
 
 /// @brief Processes function for RPC call "setLedMode"
 /// RPC_Data is a JSON variant, that can be queried using operator[]
 /// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
 /// @param data Data containing the rpc data that was called and its current value
 /// @return Response that should be sent to the cloud. Useful for getMethods
-RPC_Response processSetLedMode(const RPC_Data &data) {
+RPC_Response processSetLedMode(const RPC_Data &data)
+{
   Serial.println("Received the set led state RPC method");
 
   // Process data
   int new_mode = data;
 
-  Serial.print("Mode to change: ");
-  Serial.println(new_mode);
+ // Serial.print("Mode to change: ");
+ // Serial.println(new_mode);
 
-  if (new_mode != 0 && new_mode != 1) {
+  if (new_mode != 0 && new_mode != 1)
+  {
     StaticJsonDocument<200> doc;
     doc["error"] = "Unknown mode!";
-    //return RPC_Response(doc.as<JsonVariant>());
+    // return RPC_Response(doc.as<JsonVariant>());
     return RPC_Response(doc.as<JsonVariant>());
   }
 
@@ -229,29 +262,31 @@ RPC_Response processSetLedMode(const RPC_Data &data) {
 }
 
 
-// Optional, keep subscribed shared attributes empty instead,
-// and the callback will be called for every shared attribute changed on the device,
-// instead of only the one that were entered instead
-const std::array<RPC_Callback, 1U> callbacks = {
-  RPC_Callback{ "setLedMode", processSetLedMode }
-};
-
 
 /// @brief Update callback that will be called as soon as one of the provided shared attributes changes value,
 /// if none are provided we subscribe to any shared attribute change instead
 /// @param data Data containing the shared attributes that were changed and their current value
-void processSharedAttributes(const Shared_Attribute_Data &data) {
-  for (auto it = data.begin(); it != data.end(); ++it) {
-    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0) {
+void processSharedAttributes(const Shared_Attribute_Data &data)
+{
+  for (auto it = data.begin(); it != data.end(); ++it)
+  {
+    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0)
+    {
       const uint16_t new_interval = it->value().as<uint16_t>();
-      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX) {
+      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX)
+      {
         blinkingInterval = new_interval;
         Serial.print("Updated blinking interval to: ");
         Serial.println(new_interval);
       }
-    } else if(strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
+    }
+    else if (strcmp(it->key().c_str(), LED_STATE_ATTR) == 0)
+    {
       ledState = it->value().as<bool>();
       digitalWrite(LEDINBUILT, ledState ? HIGH : LOW);
+      digitalWrite(PIN_GREEN, ledState ? HIGH : LOW);
+      digitalWrite(PIN_BLUE, !ledState ? HIGH : LOW);
+      digitalWrite(PIN_RED, ledState ? HIGH : LOW);
       Serial.print("Updated state to: ");
       Serial.println(ledState);
     }
@@ -259,9 +294,12 @@ void processSharedAttributes(const Shared_Attribute_Data &data) {
   attributesChanged = true;
 }
 
-void processClientAttributes(const Shared_Attribute_Data &data) {
-  for (auto it = data.begin(); it != data.end(); ++it) {
-    if (strcmp(it->key().c_str(), LED_MODE_ATTR) == 0) {
+void processClientAttributes(const Shared_Attribute_Data &data)
+{
+  for (auto it = data.begin(); it != data.end(); ++it)
+  {
+    if (strcmp(it->key().c_str(), LED_MODE_ATTR) == 0)
+    {
       const uint16_t new_mode = it->value().as<uint16_t>();
       ledMode = new_mode;
     }
@@ -273,6 +311,98 @@ const Attribute_Request_Callback attribute_shared_request_callback(SHARED_ATTRIB
 const Attribute_Request_Callback attribute_client_request_callback(CLIENT_ATTRIBUTES_LIST.cbegin(), CLIENT_ATTRIBUTES_LIST.cend(), &processClientAttributes);
 
 
+RPC_Response processStateChange(const RPC_Data &data)
+{
+  Serial.println("Received the set state RPC method");
+
+  int nextstate = data["state"];
+
+  switch (nextstate)
+  {
+  case 0:
+    motorStop();
+    break;
+
+  case 1:
+    motorForward();
+    break;
+
+  case 2:
+    motorBackward();
+    break;
+
+  default:
+    Serial.println("Invalid RPC");
+  }
+  Serial.println("Current state");
+  Serial.println(nextstate);
+
+  tb.sendTelemetryInt("State", nextstate);
+  if (nextstate == 0)
+  {
+    curState = 1;
+  }
+  else
+  {
+    curState = nextstate;
+  }
+
+  StaticJsonDocument<200> doc;
+  doc["State"] = curState;
+  return RPC_Response(doc.as<JsonVariant>());
+}
+
+RPC_Response processSpeedChange(const RPC_Data &data)
+{
+  Serial.println("Received the set speed method");
+
+  // Process data
+
+  // int speed = data["speed"];
+  int speed = data;
+  setDuty(speed);
+  tb.sendTelemetryInt("Speed", speed);
+  StaticJsonDocument<200> doc;
+  doc["motorEdgeSpeed"] = speed;
+  return RPC_Response(doc.as<JsonVariant>());
+}
+
+// const std::array<RPC_Callback, 1U> callbacks1 = {
+//     RPC_Callback{"motorState", processStateChange}};
+// const std::array<RPC_Callback, 1U> callbacks2 = {
+//     RPC_Callback{"motorSpeed", processSpeedChange}};
+
+// const size_t callbacks_size = 2;
+// RPC_Callback callbacks[callbacks_size] = {
+//     {"motorState", processStateChange},
+//     {"motorSpeed", processSpeedChange},
+//     {"setLedMode", processSetLedMode}
+// };
+
+
+// Optional, keep subscribed shared attributes empty instead,
+// and the callback will be called for every shared attribute changed on the device,
+// instead of only the one that were entered instead
+const std::array<RPC_Callback, callbacks_size> callbacks = {
+    RPC_Callback{"setLedMode", processSetLedMode},
+    RPC_Callback{"motorState", processStateChange},
+    RPC_Callback{"motorSpeed", processSpeedChange}
+    };
+
+
+void setup()
+{
+  // Increase internal buffer size after inital creation.
+  tb.setBufferSize(128);
+
+  Serial.begin(115200);
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  InitWiFi();
+  InitLeds();
+  InitMotors();
+}
+
 void loop()
 {
   if (WiFi.status() != WL_CONNECTED)
@@ -281,7 +411,7 @@ void loop()
     reconnect();
   }
 
-  Serial.println(WiFi.status());
+  //Serial.println(WiFi.status());
 
   if (!tb.connected())
   {
@@ -296,43 +426,54 @@ void loop()
       Serial.println("Failed to connect");
       return;
     }
+    // Sending a MAC address as an attribute
+    tb.sendAttributeString("macAddress", WiFi.macAddress().c_str());
   }
 
-////
-if (!subscribed) {
+  ////
+  if (!subscribed)
+  {
     Serial.println("Subscribing for RPC...");
     // Perform a subscription. All consequent data processing will happen in
     // processSetLedState() and processSetLedMode() functions,
     // as denoted by callbacks array.
-    if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
+    if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend()))
+    {
       Serial.println("Failed to subscribe for RPC");
       return;
     }
 
-    if (!tb.Shared_Attributes_Subscribe(attributes_callback)) {
+    if (!tb.Shared_Attributes_Subscribe(attributes_callback))
+    {
       Serial.println("Failed to subscribe for shared attribute updates");
       return;
     }
 
-    Serial.println("Subscribe done");
-    subscribed = true;
+    
 
     // Request current states of shared attributes
-    if (!tb.Shared_Attributes_Request(attribute_shared_request_callback)) {
+    if (!tb.Shared_Attributes_Request(attribute_shared_request_callback))
+    {
       Serial.println("Failed to request for shared attributes");
       return;
     }
 
     // Request current states of client attributes
-    if (!tb.Client_Attributes_Request(attribute_client_request_callback)) {
+    if (!tb.Client_Attributes_Request(attribute_client_request_callback))
+    {
       Serial.println("Failed to request for client attributes");
       return;
     }
+
+    Serial.println("Subscribe done");
+    subscribed = true;
   }
 
-  if (attributesChanged) {
+  if (attributesChanged)
+  {
     attributesChanged = false;
-    if (ledMode == 0) {
+    if (ledMode == 0)
+    {
       previousStateChange = millis();
     }
     tb.sendTelemetryInt(LED_MODE_ATTR, ledMode);
@@ -341,103 +482,90 @@ if (!subscribed) {
     tb.sendAttributeBool(LED_STATE_ATTR, ledState);
   }
 
-  if (ledMode == 1 && millis() - previousStateChange > blinkingInterval) {
+  if (ledMode == 1 && millis() - previousStateChange > blinkingInterval)
+  {
     previousStateChange = millis();
     ledState = !ledState;
     digitalWrite(LEDINBUILT, ledState);
     tb.sendTelemetryBool(LED_STATE_ATTR, ledState);
     tb.sendAttributeBool(LED_STATE_ATTR, ledState);
-    if (LEDINBUILT == 99) {
+    if (LEDINBUILT == 99)
+    {
       Serial.print("LED state changed to: ");
-      Serial.println(ledState);
+      //Serial.println(ledState);
     }
   }
 
-////
-
+  ////
 
   sendDataToThingsBoard();
-  // if (!subscribed) {
-  //   Serial.println("Subscribing for RPC...");
 
-  //   // Perform a subscription. All consequent data processing will happen in
-  //   // processTemperatureChange() and processSwitchChange() functions,
-  //   // as denoted by callbacks[] array.
-  //   if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
-  //     Serial.println("Failed to subscribe for RPC");
-  //     return;
-  //   }
+  // // blue
+  // setColor(0, 214, 102);
 
-  //   Serial.println("Subscribe done");
-  //   subscribed = true;
-  // }
+  // // Move the DC motor forward at maximum speed
+  // Serial.println("Moving Forward");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, HIGH);
 
-  // blue
-  setColor(0, 214, 102);
+  // // LEDs
+  // digitalWrite(LEDINBUILT, HIGH);
+  // delay(2000);
 
-  // Move the DC motor forward at maximum speed
-  Serial.println("Moving Forward");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, HIGH);
+  // // Stop the DC motor
+  // Serial.println("Motor stopped");
 
-  // LEDs
-  digitalWrite(LEDINBUILT, HIGH);
-  delay(2000);
+  // // red
+  // setColor(247, 120, 138);
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, LOW);
 
-  // Stop the DC motor
-  Serial.println("Motor stopped");
+  // delay(2000);
 
-  // red
-  setColor(247, 120, 138);
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-
-  delay(2000);
-
-  // Move DC motor backwards at maximum speed
-  // yellow
-  setColor(255, 227, 22);
-  Serial.println("Moving Backwards");
-  digitalWrite(motor1Pin1, HIGH);
-  digitalWrite(motor1Pin2, LOW);
-
-  delay(2000);
-
-  // red
-  setColor(247, 120, 138);
-  // Stop the DC motor
-  Serial.println("Motor stopped");
-  digitalWrite(motor1Pin1, LOW);
-  digitalWrite(motor1Pin2, LOW);
-
-  delay(2000);
-  digitalWrite(LEDINBUILT, LOW);
-
-  // green
-  setColor(62, 214, 102);
-  // Move DC motor forward with increasing speed
+  // // Move DC motor backwards at maximum speed
+  // // yellow
+  // setColor(255, 227, 22);
+  // Serial.println("Moving Backwards");
   // digitalWrite(motor1Pin1, HIGH);
   // digitalWrite(motor1Pin2, LOW);
 
-  motor.begin();
-  motor.SetMotorSpeed(250);
-  delay(5000);
-  motor.Stop();
+  // delay(2000);
 
-  while (dutyCycle <= 255)
-  {
-    ledcWrite(pwmChannel, dutyCycle);
-    Serial.print("Forward with duty cycle: ");
-    Serial.println(dutyCycle);
-    // green
-    setColor(62, 214, 102);
-    dutyCycle = dutyCycle + 5;
-    delay(500);
-  }
-  dutyCycle = 200;
-  // yellow
-  setColor(255, 227, 22);
-  delay(500);
+  // // red
+  // setColor(247, 120, 138);
+  // // Stop the DC motor
+  // Serial.println("Motor stopped");
+  // digitalWrite(motor1Pin1, LOW);
+  // digitalWrite(motor1Pin2, LOW);
+
+  // delay(2000);
+  // digitalWrite(LEDINBUILT, LOW);
+
+  // // green
+  // setColor(62, 214, 102);
+  // // Move DC motor forward with increasing speed
+  // // digitalWrite(motor1Pin1, HIGH);
+  // // digitalWrite(motor1Pin2, LOW);
+
+  // motor.begin();
+  // motor.SetMotorSpeed(250);
+  // delay(5000);
+  // motor.Stop();
+
+  // while (dutyCycle <= 255)
+  // {
+  //   ledcWrite(pwmChannel, dutyCycle);
+  //   Serial.print("Forward with duty cycle: ");
+  //   Serial.println(dutyCycle);
+  //   // green
+  //   setColor(62, 214, 102);
+  //   dutyCycle = dutyCycle + 5;
+  //   delay(500);
+  // }
+  // dutyCycle = 200;
+  // // yellow
+  // setColor(255, 227, 22);
+  // delay(500);
 
   tb.loop();
 }
